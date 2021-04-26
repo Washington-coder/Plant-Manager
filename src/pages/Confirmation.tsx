2494fdf8dd652725ac90dd24b695612140e0c723
Washington-coder <was.antonio.xy@gmail.com>
2021-04-25 21:56:06 -0400

    Edits and Up

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -4,25 +4,46 @@ import { StyleSheet, SafeAreaView, Text, View } from "react-native";
 import { Button } from "../components/Button";
 import colors from "../styles/colors";
 import fonts from "../styles/fonts";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+
+interface Params {
+  title: string;
+  subtitle: string;
+  buttonTitle: string;
+  icon: "smile" | "hug";
+  nextScreen: string;
+}
+
+const emojis = {
+  hug: "😉",
+  smile: "😎",
+};
 
 export function Confirmation() {
   const navigation = useNavigation();
+  const routes = useRoute();
+
+  const {
+    title,
+    subtitle,
+    buttonTitle,
+    icon,
+    nextScreen,
+  } = routes.params as Params;
+
   function handleMoveOn() {
-    navigation.navigate("PlantSelect");
+    navigation.navigate(nextScreen);
   }
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
-        <Text style={styles.emoji}>😎</Text>
+        <Text style={styles.emoji}>{emojis[icon]}</Text>
 
-        <Text style={styles.title}>Prontinho.</Text>
+        <Text style={styles.title}>{title}</Text>
 
-        <Text style={styles.subtitle}>
-          Agora vamos começar a cuidar das suas plantinhas com muito cuidado.
-        </Text>
+        <Text style={styles.subtitle}>{subtitle}</Text>
         <View style={styles.footer}>
-          <Button title="Começar" onPress={handleMoveOn} />
+          <Button title={buttonTitle} onPress={handleMoveOn} />
         </View>
       </View>
     </SafeAreaView>
--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -14,11 +14,11 @@ import waterdrop from "../assets/waterdrop.png";
 import { Button } from "../components/Button";
 import colors from "../styles/colors";
 import { getBottomSpace } from "react-native-iphone-x-helper";
-import { useRoute } from "@react-navigation/core";
+import { useRoute, useNavigation } from "@react-navigation/core";
 import DateTimePicker, { Event } from "@react-native-community/datetimepicker";
 import { isBefore, format } from "date-fns";
 import fonts from "../styles/fonts";
-import { PlantProps, savePlant, loadPlant } from "../libs/storage";
+import { PlantProps, savePlant } from "../libs/storage";
 
 interface Params {
   plant: PlantProps;
@@ -29,6 +29,8 @@ export function PlantSave() {
   const [showDatePicker, setShowDatePicker] = useState(Platform.OS == "ios");
   const route = useRoute();
   const { plant } = route.params as Params;
+
+  const navigation = useNavigation();
 
   function handleChangeTime(event: Event, dateTime: Date | undefined) {
     if (Platform.OS == "android") {
@@ -50,17 +52,22 @@ export function PlantSave() {
   }
 
   async function handleSave() {
-    const data = await loadPlant();
-    console.log(data);
-
-    // try {
-    //   await savePlant({
-    //     ...plant,
-    //     dateTimeNotification: seletedDateTime,
-    //   });
-    // } catch {
-    //   Alert.alert("Não foi possível salvar.😔");
-    // }
+    try {
+      await savePlant({
+        ...plant,
+        dateTimeNotification: seletedDateTime,
+      });
+      navigation.navigate("Confirmation", {
+        title: "Tudo certo.",
+        subtitle:
+          "Fique tranquilo que sempre vamos lembrar você de cuidar da sua plantinha com muito cuidado.",
+        buttonTitle: "Muito Obrigado :D",
+        icon: "hug",
+        nextScreen: "MyPlants",
+      });
+    } catch {
+      Alert.alert("Não foi possível salvar.😔");
+    }
   }
 
   return (
